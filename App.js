7c7ec5918588fc26c2bd3f90e3365d4b59381413
Jesper Paardekooper <j.paardekooper@rinkel.nl>
2024-05-17 16:23:52 +0200

    showcase

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import * as Contacts from 'expo-contacts';
+import {useEffect, useState} from "react";
 
 export default function App() {
+  const [permission, setPermission] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await Contacts.getPermissionsAsync();
+      setPermission(status === 'granted');
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!permission && (
+        <Button title="Give permission" onPress={async () => {
+          const result = await Contacts.requestPermissionsAsync();
+          if (result.status === 'granted') {
+            setPermission(true);
+          }
+        }}/>)}
+      {permission && (
+        <Button title="Add contact" onPress={async () => {
+          console.log('add contact');
+
+          await Contacts.presentFormAsync(
+            null,
+            {
+              contactType: 'person',
+              name: '',
+            },
+            {
+              isNew: true,
+            }
+          );
+
+          console.log('done');
+        }}/>
+      )}
+      <StatusBar style="auto"/>
     </View>
   );
 }
